b87441143282fec92e967b7e36febda45bb7c8c3
brcds <brunocristianocontato@gmail.com>
2025-06-27 00:41:11 -0300

    adding statics, create_pix and README.md

--- db_models/payment.py
+++ db_models/payment.py
@@ -5,6 +5,17 @@ class Payment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Float)
     paid = db.Column(db.Boolean, default=False)
-    bank_payment_id = db.Column(db.Integer, nullable=True)
+    bank_payment_id = db.Column(db.String(200), nullable=True)
     qr_code = db.Column(db.String(100), nullable=True)
     expiration_date = db.Column(db.DateTime)
+
+
+    def to_dict(self):
+        return {
+            "id":self.id,
+            "value":self.value,
+            "paid": self.paid,
+            "bankPaymentId": self.bank_payment_id,
+            "qrCode": self.qr_code,
+            "expirationDate": self.expiration_date
+        }
