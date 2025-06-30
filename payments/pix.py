2a26959eef03d107a9208279a7ce8f96b8110527
brcds <brunocristianocontato@gmail.com>
2025-06-29 22:44:34 -0300

    adding: tests

--- payments/pix.py
+++ payments/pix.py
@@ -6,15 +6,15 @@ class Pix:
     def __init__(self):
         pass
 
-    def create_payment(self):
+    def create_payment(self, base_dir=""):
         # criar o pagamento na instituicao financeira
         bank_payment_id = str(uuid.uuid4())
         # codigo copia e cola.
-        hash_payment =  f'hash_payment_{bank_payment_id}'
+        hash_payment = f'hash_payment_{bank_payment_id}'
         # qr code
         img = qrcode.make(hash_payment)
         # salva a img como arquivo PNG
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
         return {
             "payment_bank_id": bank_payment_id,
             "qr_code_path": f"qr_code_payment_{bank_payment_id}"
